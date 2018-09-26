263062e2d8bd9169cfd4500fd02374f18a98c8bc
Noah Brenner <noah@noahbrenner.com>
2018-09-25 23:35:57 -0700

    Make "back" link work in more situations
    The link will now work when there is no referrer (because the user typed
    a URL in manually) but there is a history, and thus a page to go back
    to. In this case, the URL shown in the href attribute will just be the
    current page (since we can't access the URL from the history), but
    clicking on the link will still go back a page in the history.

--- instrument_catalog/static/scripts.js
+++ instrument_catalog/static/scripts.js
@@ -2,9 +2,11 @@
 (function () {
     // Thanks for inspiration goes to: https://stackoverflow.com/a/46163215
     var backLink = document.getElementById('back-link');
-    if (backLink && document.referrer) {
+    if (backLink && window.history.length > 1) {
         // Show the URL that the user would go back to
-        backLink.href = document.referrer;
+        // We might not have a referrer if the user manually typed in the URL
+        backLink.href = document.referrer || '#';
+
         // Go back using the history instead of following the link forward
         backLink.onclick = function (event) {
             event.preventDefault();
