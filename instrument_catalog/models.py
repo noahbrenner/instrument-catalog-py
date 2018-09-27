a3755ef359ce54fc66333c9d45c08ec2dc32d01b
Noah Brenner <noah@noahbrenner.com>
2018-09-27 14:39:42 -0700

    Adjust database table definitions

--- instrument_catalog/models.py
+++ instrument_catalog/models.py
@@ -6,44 +6,42 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), unique=True, nullable=False)
 
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    description = db.Column(db.String(10000), nullable=False)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    description = db.Column(db.String(16384), nullable=False)
 
 
-class AlternateInstrumentNames(db.Model):
+class AlternateInstrumentName(db.Model):
     __table_args__ = (
-        # Alternate names must be unique relative to a given instrument
-        db.UniqueConstraint('instrument_id', 'name', name='_instrument_name'),
-        # Each name must have a unique sort order for a given instrument
-        db.UniqueConstraint('instrument_id', 'index', name='_instrument_index')
+        # Each name has a unique sort order for a given instrument
+        db.UniqueConstraint('instrument_id', 'index',
+                            name='unique_instrument_index'),
     )
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    index = db.Column(db.Integer)  # Used for display ordering
-
-    instrument_id = db.Column(db.Integer, db.ForeignKey('instrument.id'),
-                              nullable=False)
+    instrument_id = db.Column(db.Integer,
+                              db.ForeignKey('instrument.id'), primary_key=True)
+    name = db.Column(db.String(128), primary_key=True)
+    index = db.Column(db.SmallInteger)  # Used for display ordering
 
 
 class Instrument(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    description = db.Column(db.String(10000), nullable=False)
-    image = db.Column(db.String(500))
+    name = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(16384), nullable=False)
+    image = db.Column(db.String(512))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'),
                             nullable=False)
     category = db.relationship('Category', backref='instruments', lazy=True)
 
-    alternate_names = db.relationship('AlternateInstrumentNames', lazy=False)
+    alternate_names = db.relationship('AlternateInstrumentName', lazy=False,
+                                      order_by='AlternateInstrumentName.index')
 
     def iter_alt_names(self):
         for alternate in self.alternate_names:
