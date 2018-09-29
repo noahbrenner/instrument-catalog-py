cfb49b573f0a8c4c292499f6f4508b71f9987893
Noah Brenner <noah@noahbrenner.com>
2018-09-28 17:35:33 -0700

    Use camelCase for HTML id attributes

--- instrument_catalog/static/scripts.js
+++ instrument_catalog/static/scripts.js
@@ -1,7 +1,7 @@
 // Add "back" functionality if the current page has a link with id 'back-link'
 (function () {
     // Thanks for inspiration goes to: https://stackoverflow.com/a/46163215
-    var backLink = document.getElementById('back-link');
+    var backLink = document.getElementById('backLink');
     if (backLink && window.history.length > 1) {
         // Show the URL that the user would go back to
         // We might not have a referrer if the user manually typed in the URL
