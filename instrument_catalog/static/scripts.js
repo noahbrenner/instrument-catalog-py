ab4868816aaca8dbdb208ff76f73692529862a45
Noah Brenner <noah@noahbrenner.com>
2018-10-02 13:50:52 -0700

    Refactor scripts.js for clarity and DRY-ness
    * Combine all instrument form logic inside a single IIFE.
    * Clarify and format comments.
    * Use `addEventListener()` instead of `element.on...` property.

--- instrument_catalog/static/scripts.js
+++ instrument_catalog/static/scripts.js
@@ -1,52 +1,54 @@
 'use strict';
 
-// Add "back" functionality if the current page has a link with id 'back-link'
+// Add "back" functionality when the page has an element with id="back-link"
+// Thanks to https://stackoverflow.com/a/46163215 for inspiration
 (function () {
-    // Thanks for inspiration goes to: https://stackoverflow.com/a/46163215
     var backLink = document.getElementById('backLink');
     if (backLink && window.history.length > 1) {
-        // Show the URL that the user would go back to
-        // We might not have a referrer if the user manually typed in the URL
+        // Show the URL that the user would go back to, if possible (we might
+        // not have access to the referrer even if there is a browser history)
         backLink.href = document.referrer || '#';
 
         // Go back using the history instead of following the link forward
-        backLink.onclick = function (event) {
+        backLink.addEventListener('click', function (event) {
             event.preventDefault();
             window.history.back();
-        };
-    }
-})();
-
-
-// Prefill instrument-editing form with the current values
-(function () {
-    if (window.instrument && document.forms.instrumentForm) {
-        var instrument = window.instrument;
-        var fields = document.forms.instrumentForm.elements;
-
-        // Set form values
-        fields.name.value = instrument.name;
-        fields.altNames.value = instrument.alternate_names.join('\n');
-        fields['category' + instrument.category_id].checked = true;
-        fields.image.value = instrument.image;
-        fields.description.value = instrument.description;
-    }
-})();
-
-
-// Display custom message for invalid form input
-(function () {
-    if (document.forms.instrumentForm) {
-        var imageElement = document.forms.instrumentForm.elements.image;
-        var validity = imageElement.validity;
-        var errMessage = 'Please enter a URL starting with "https" or "http".';
-
-        imageElement.addEventListener('input', function (event) {
-            if (validity.typeMismatch || validity.patternMismatch) {
-                imageElement.setCustomValidity(errMessage);
-            } else {
-                imageElement.setCustomValidity('');
-            }
         });
     }
+})();
+
+
+// Instrument form functionality
+(function () {
+    var form = document.forms.instrumentForm;
+    var formFields;
+
+    if (!form) {
+        return;
+    }
+
+    formFields = form.elements;
+
+    // Prefill instrument-editing form with the current values
+    // (`window.instrument` is only populated on the "edit instrument" page)
+    if (window.instrument) {
+        formFields.name.value = window.instrument.name;
+        formFields.altNames.value = window.instrument.alternate_names.join('\n');
+        formFields['category' + window.instrument.category_id].checked = true;
+        formFields.image.value = window.instrument.image;
+        formFields.description.value = window.instrument.description;
+    }
+
+    // Display custom message for invalid form input
+    formFields.image.addEventListener('input', function (event) {
+        var element = event.target;
+
+        if (element.validity.typeMismatch || element.validity.patternMismatch) {
+            element.setCustomValidity(
+                'Please enter a URL starting with "https" or "http".'
+            );
+        } else {
+            element.setCustomValidity('');
+        }
+    });
 })();
