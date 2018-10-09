1d81e2519cd9675cf6df2dd8e7fbcd60ffd3299a
Noah Brenner <noah@noahbrenner.com>
2018-10-08 20:31:12 -0700

    Adjust signature of `api_jsonify()` in `api.py`
    This commit also includes refactoring of code that uses api_jsonify().

--- instrument_catalog/api.py
+++ instrument_catalog/api.py
@@ -22,43 +22,42 @@ def auth_stub():
         g.user = User.query.get(user_id)
 
 
-def api_jsonify(errors, data):
+def api_jsonify(data, errors=None):
+    """Return a standardized, JSON-ified response."""
+    errors = [] if errors is None else errors
     return jsonify(successful=not errors, errors=errors, data=data)
 
 
 @bp.route('/categories/')
 def categories_api():
     """API endpoint representing all categories."""
-    errors = []
-    categories_data = [c.serialize() for c in Category.query]
-    return api_jsonify(errors, categories_data)
+    all_categories = [c.serialize() for c in Category.query]
+    return api_jsonify(all_categories)
 
 
 @bp.route('/categories/<int:category_id>/')
 def one_category_api(category_id):
     """API endpoint for a single category."""
-    errors = []
-    category_data = Category.query.get(category_id).serialize()
-    return api_jsonify(errors, category_data)
+    one_category = Category.query.get(category_id).serialize()
+    return api_jsonify(one_category)
 
 
 @bp.route('/categories/<int:category_id>/instruments/')
 def one_category_instruments_api(category_id):
     """API endpoint for instruments in a single category."""
-    errors = []
-    instruments_data = [i.serialize() for i in Instrument.query.filter_by(
-                        category_id=category_id)]
-    return api_jsonify(errors, instruments_data)
+    category_instruments = [
+        instrument.serialize()
+        for instrument in Instrument.query.filter_by(category_id=category_id)]
+
+    return api_jsonify(category_instruments)
 
 
 @bp.route('/instruments/', methods=('GET', 'POST'))
 def instruments_api():
     """API endpoint for creating or listing instruments."""
-    errors = []
-
     if request.method == 'GET':
-        instruments_data = [i.serialize() for i in Instrument.query]
-        return api_jsonify(errors, instruments_data)
+        all_instruments = [i.serialize() for i in Instrument.query]
+        return api_jsonify(all_instruments)
 
     elif request.method == 'POST':
         pass
@@ -67,11 +66,10 @@ def instruments_api():
 @bp.route('/instruments/<int:instrument_id>/',
           methods=('GET', 'PUT', 'DELETE'))
 def one_instrument_api(instrument_id):
-    errors = []
     instrument = Instrument.query.get(instrument_id)
 
     if request.method == 'GET':
-        return api_jsonify(errors, instrument.serialize())
+        return api_jsonify(instrument.serialize())
 
     elif request.method == 'PUT':
         pass
@@ -82,8 +80,8 @@ def one_instrument_api(instrument_id):
 
 @bp.route('/myinstruments/')
 def my_instruments_api():
-    errors = []
-    instruments_data = [i.serialize() for i in Instrument.query.filter_by(
-                        user_id=g.user.id)]
+    user_instruments = [
+        instrument.serialize()
+        for instrument in Instrument.query.filter_by(user_id=g.user.id)]
 
-    return api_jsonify(errors, instruments_data)
+    return api_jsonify(user_instruments)
